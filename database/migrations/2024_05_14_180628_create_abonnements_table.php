a696342d32678cf173043483cdf58831ec95b700
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-14 19:41:59 +0100

    migration abonnement

--- database/migrations/2024_05_14_180628_create_abonnements_table.php
+++ database/migrations/2024_05_14_180628_create_abonnements_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('abonnements', function (Blueprint $table) {
             $table->id();
+            $table->text('nom');
+            $table->text('prenom');
+            $table->text('email');
+            $table->integer('phone');
+            $table->text('lieu');
+            $table->date('datedebut');
+            $table->date('datefin');
+            $table->string('garedepart');
+            $table->string('garearrive');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
